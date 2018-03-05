822c587bea9d2dbe9f879931ece35fd83a8bd75f
husaar <lukasz.kacprzak198@gmail.com>
2018-03-05 10:11:42 +0100

    bacis click event logic for generate button
    - reading number from textbox (numbers of instances sets to be generated).
    - Exception handling for number provided by the user.
    - ParseStringToInteger() method created

--- BPMbenchmarksGenerator/MainWindow.xaml.cs
+++ BPMbenchmarksGenerator/MainWindow.xaml.cs
@@ -27,7 +27,46 @@ namespace BPMbenchmarksGenerator
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            string stringInstancesSetsNumber = txtInstancesSetsNumber.Text;
+            
+            int intInstancesSetsNumber = -1;
+            try
+            {
+                intInstancesSetsNumber = ParseStringToInteger(stringInstancesSetsNumber, 1);
+                MessageBox.Show("stringInstancesSetsNumber: ", stringInstancesSetsNumber);
+            }
+            catch(FormatException fex)
+            {
+                MessageBox.Show(fex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
+        }
+
+        private int ParseStringToInteger(string s, int lowestPossibleNumber)
+        {
+            int integer = -2;
+            string exceptionMessage = null;
+            try
+            {
+                integer = int.Parse(s);
+            }
+            catch(FormatException)
+            {
+                exceptionMessage = "Provided number has to be integer!";
+                throw new FormatException(exceptionMessage);
+            }
+
+            if(integer < lowestPossibleNumber)
+            {
+                exceptionMessage = string.Format($"Integer value should be at least {lowestPossibleNumber.ToString()}.");
+                throw new Exception(exceptionMessage);
+            }
+
+            return integer;
         }
     }
 }
