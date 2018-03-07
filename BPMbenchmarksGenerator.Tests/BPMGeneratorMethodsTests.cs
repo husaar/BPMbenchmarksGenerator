a0510a141e36099e0b5fc693b2a148a8e76fc5ab
husaar <lukasz.kacprzak198@gmail.com>
2018-03-07 09:27:50 +0100

    BPMGeneratorMethodsTests added
    BPMGeneratorMethodsTests added

--- BPMbenchmarksGenerator.Tests/BPMGeneratorMethodsTests.cs
+++ BPMbenchmarksGenerator.Tests/BPMGeneratorMethodsTests.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
-using FluentAssertions;
-using Moq;
-using BPMbenchmarksGenerator;
 
 namespace BPMbenchmarksGenerator.Tests
 {
-    [TestFixture]
-    public class BPMbenchmarksGeneratorTests
+    class BPMGeneratorMethodsTests
     {
-        
     }
 }
