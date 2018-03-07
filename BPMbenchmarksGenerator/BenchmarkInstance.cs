895e49442c300093d2dcaeb737141dcc4adee035
husaar <lukasz.kacprzak198@gmail.com>
2018-03-07 10:19:26 +0100

    BenchmarkInstance and JobParameters classes added
    BenchmarkInstance and JobParameters classes added

---
+++ BPMbenchmarksGenerator/BenchmarkInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMbenchmarksGenerator
+{
+    class BenchmarkInstance
+    {
+        public int NumberOfJobs { get; }
+        public int MachineCapacity { get; }
+
+        private readonly List<JobParameters> jobsList;
+
+        public List<JobParameters> JobsList
+        {
+            get { return this.jobsList; }
+        }
+
+        public BenchmarkInstance(int numberOfJobs, int machineCapacity, List<JobParameters> jobList)
+        {
+            NumberOfJobs = numberOfJobs;
+            MachineCapacity = machineCapacity;
+            jobsList = jobList;
+        }
+    }
+}
---
+++ BPMbenchmarksGenerator/JobParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMbenchmarksGenerator
+{
+    class JobParameters
+    {
+        public int JobProcessingTime { get; }
+        public int JobSize { get; }
+
+        public JobParameters(int jobProcessingTime, int jobSize)
+        {
+            JobProcessingTime = jobProcessingTime;
+            JobSize = jobSize;
+        }
+    }
+}
